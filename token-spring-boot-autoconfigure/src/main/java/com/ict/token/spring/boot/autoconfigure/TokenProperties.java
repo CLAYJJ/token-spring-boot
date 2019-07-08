0db93735d5850f69ecb87570440d839c15205fa6
cyberdriver <1198201587@qq.com>
2019-07-08 10:19:27 +0800

    添加enable校验字段

--- token-spring-boot-autoconfigure/src/main/java/com/ict/token/spring/boot/autoconfigure/TokenProperties.java
+++ token-spring-boot-autoconfigure/src/main/java/com/ict/token/spring/boot/autoconfigure/TokenProperties.java
@@ -1,7 +1,9 @@
 package com.ict.token.spring.boot.autoconfigure;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 /**
@@ -10,15 +12,30 @@ import java.util.List;
  * @since 1.1.1
  */
 @ConfigurationProperties(prefix = "token")
+@Validated
 public class TokenProperties {
     /**
-     * 需要token验证的路径
+     * 是否开启token功能,默认为false
      */
+    private boolean enable = false;
+    /**
+     * 需要token验证的路径，使用过滤器的urlPattern的写法，
+     * 在配置了enable参数为true时必须配置此项
+     */
+    @NotNull
     private List<String> includes;
     /**
      * 不需要token验证的路径，如果此路径不包含在includes中的话就不要配置
      */
     private List<String> excludes;
+
+    public boolean isEnable() {
+        return enable;
+    }
+
+    public void setEnable(boolean enable) {
+        this.enable = enable;
+    }
 
     public List<String> getIncludes() {
         return includes;
--- token-spring-boot-autoconfigure/src/main/java/com/ict/token/spring/boot/autoconfigure/conditions/TokenCondition.java
+++ token-spring-boot-autoconfigure/src/main/java/com/ict/token/spring/boot/autoconfigure/conditions/TokenCondition.java
@@ -5,16 +5,21 @@ import org.springframework.boot.autoconfigure.condition.SpringBootCondition;
 import org.springframework.context.annotation.ConditionContext;
 import org.springframework.core.env.Environment;
 import org.springframework.core.type.AnnotatedTypeMetadata;
-/*
- * token配置条件：配置了token.includes就满足条件，否则不满足
+/**
+ * token配置条件：配置了token.enable = true和token.includes就满足条件，否则不满足
  * @author Dante
  */
 public class TokenCondition extends SpringBootCondition {
+
+    private final String TOKEN_ENABLE = "token.enable";
     @Override
     public ConditionOutcome getMatchOutcome(ConditionContext context, AnnotatedTypeMetadata metadata) {
         Environment environment = context.getEnvironment();
-        if (environment.containsProperty("token.includes[0]"))
-            return ConditionOutcome.match();
-        return ConditionOutcome.noMatch("token.includes未配置");
+        if (Boolean.TRUE.toString().equals(environment.getProperty(TOKEN_ENABLE)))
+                return ConditionOutcome.match();
+        return ConditionOutcome.noMatch(TOKEN_ENABLE+":未配置或者配置为false");
+
     }
+
+
 }
